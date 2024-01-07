70299d115cea3243b603fc756f043f6c1ac0b7a6
Kirill Glinskikh <glinskix24@bk.ru>
2024-01-07 19:06:37 +0300

    конфигурация приложения

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,31 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"grpc-service-ref/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
-	
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
 }
